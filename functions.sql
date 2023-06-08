528169f98678005244aa58c0fced6139110637e3
Santos Braun <69260170+CakeH4X@users.noreply.github.com>
2023-06-08 20:47:48 -0300

    Update functions.sql
    Buenas noches

--- functions.sql
+++ functions.sql
@@ -3,15 +3,13 @@ DROP TABLE IF EXISTS anio CASCADE;
 DROP TABLE IF EXISTS nivel_educacion CASCADE;
 DROP TABLE IF EXISTS temp_table CASCADE;
 
-COPY temp_table FROM '/Users/tmmymrtnz/Documents_Local/TP_BD/us_births_2016_2021.csv' WITH (FORMAT csv, HEADER true, DELIMITER ',');
-
-CREATE TEMPORARY TABLE temp_table (
+CREATE TABLE temp_table (
         estado_desc         TEXT,
         estado_abr          CHAR(2),
         anio                INT,
         genero              CHAR,
-        edu_descr           TEXT,
-        edu_cod             INT,
+        edu_desc            TEXT,
+        edu_code            INT,
         nacimientos         INT,
         m_edad_prom         FLOAT,
         avg_birth_weight    FLOAT
@@ -30,61 +28,272 @@ CREATE TABLE anio(
 );
 
 CREATE TABLE nivel_educacion(
-        edu_cod         INT NOT NULL,
-        edu_descr       TEXT NOT NULL,
-        PRIMARY KEY(edu_cod)
+        edu_code         INT NOT NULL,
+        edu_desc       TEXT NOT NULL,
+        PRIMARY KEY(edu_code)
 );
 
-/* CARGAMOS LOS DATOS CORRESPONDIENTES DENTRO DE LA TABLA "estado" */
-INSERT INTO estado(estado_desc, estado_abr)
-SELECT temp_table.estado, temp_table.estado_abbr FROM temp_table
-ON CONFLICT (estado_abbr) DO NOTHING;
 
-/* CARGAMOS LOS DATOS CORRESPONDIENTES DENTRO DE LA TABLA "anios" */
-insert into anios(anio,es_bisiesto)
-SELECT temp_table.anio,
-        CASE
-                WHEN temp_table.anio % 4 == 0 THEN TRUE
-                WHEN temp_table.anio % 100 != 0 THEN TRUE
-                WHEN temp_table.anio % 4 != 0 THEN TRUE
-                ELSE FALSE
-        END
-FROM temp_table
-ON CONFLICT (anio) DO NOTHING;
+CREATE OR REPLACE FUNCTION insert_estado() RETURNS TRIGGER AS $$
+BEGIN
+    IF NOT EXISTS (SELECT * FROM estado WHERE estado_abr = NEW.estado_abr) THEN
+        INSERT INTO estado (estado_desc, estado_abr) VALUES (NEW.estado_desc, NEW.estado_abr);
+    END IF;
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
 
-/* CARGAMOS LOS DATOS CORRESPONDIENTES DENTRO DE LA TABLA "nivel_educacion" */
-INSERT INTO nivel_educacion(edu_code,edu_titulo)
-SELECT temp_table.edu_code,temp_table.mother_edu FROM temp_table
-ON CONFLICT (edu_code) DO NOTHING;
+CREATE OR REPLACE FUNCTION insert_anio() RETURNS TRIGGER AS $$
+BEGIN
+    IF NOT EXISTS (SELECT * FROM anio WHERE anio = NEW.anio) THEN
+        INSERT INTO anio (anio, es_bisiesto) VALUES (NEW.anio, ((NEW.anio % 4 = 0 AND NEW.anio % 100 != 0) OR NEW.anio % 400 = 0));
+    END IF;
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION insert_nivel_educacion() RETURNS TRIGGER AS $$
+BEGIN
+    IF NOT EXISTS (SELECT * FROM nivel_educacion WHERE edu_code = NEW.edu_code) THEN
+        INSERT INTO nivel_educacion (edu_code, edu_desc) VALUES (NEW.edu_code, NEW.edu_desc);
+    END IF;
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+
+DROP TRIGGER IF EXISTS insert_estado_trigger ON temp_table;
+DROP TRIGGER IF EXISTS insert_anio_trigger ON temp_table;
+DROP TRIGGER IF EXISTS insert_nivel_educacion_trigger ON temp_table;
+
+CREATE TRIGGER insert_estado_trigger
+AFTER INSERT ON temp_table
+FOR EACH ROW
+EXECUTE FUNCTION insert_estado();
+
+CREATE TRIGGER insert_anio_trigger
+AFTER INSERT ON temp_table
+FOR EACH ROW
+EXECUTE FUNCTION insert_anio();
+
+CREATE TRIGGER insert_nivel_educacion_trigger
+AFTER INSERT ON temp_table
+FOR EACH ROW
+EXECUTE FUNCTION insert_nivel_educacion();
+
+COPY temp_table FROM '/Library/PostgreSQL/15/us_births_2016_2021.csv' WITH (FORMAT csv, HEADER true, DELIMITER ',');
 
 /* ------------------------------------------------------------------------ */
 /* ----------------------- DECLARACION DE FUNCIONES ----------------------- */
 /* -------------------------- Y DATOS DE SALIDA --------------------------- */
+DROP FUNCTION IF EXISTS year_state_stats(integer);
+DROP FUNCTION IF EXISTS year_gender_stats(integer);
+DROP FUNCTION IF EXISTS year_education_stats(integer);
 DROP FUNCTION IF EXISTS reporte_consolidado;
 DROP FUNCTION IF EXISTS es_año_bisiesto;
+DROP FUNCTION IF EXISTS year_calculate_stats(aYear INTEGER);
+DROP FUNCTION IF EXISTS year_calculate_stats(aYear INTEGER);
+DROP FUNCTION IF EXISTS ReporteConsolidado(N INTEGER);
 
-CREATE year_state_record AS (
-    total INT,
-    avgage INT,
-    minage INT,
-    maxage INT,
-    avgweight DECIMAL,
-    minweight DECIMAL,
-    maxweight decimal
-);
+/* FUNCION QUE DEVUELVE PARA UN year UN QUERY QUE TIENE LA INFO DE CADA ESTADO */
 
-CREATE FUNCTION year_state_stats(IN year INT) RETURNS year_state_record AS $$
-
-DECLARE
-    result year_state_record;
+CREATE OR REPLACE FUNCTION year_state_stats(aYear INT)
+RETURNS TABLE (
+  category TEXT,
+  total BIGINT,
+  avg_age NUMERIC,
+  min_age NUMERIC,
+  max_age NUMERIC,
+  avg_weight NUMERIC,
+  min_weight NUMERIC,
+  max_weight NUMERIC
+)
+AS $$
 BEGIN
-    SELECT SUM(births) as Total, AVG(m_avg_age) as AvgAge, MIN(m_avg_age) as MinAge, MAX(m_avg_age) as MaxAge, AVG(avg_birth_weight) as AvgWeight, MIN(avg_birth_weight) as MingWeight, MAX(avg_birth_weight) as MaxWeight
-    FROM bruto
-    GROUP BY estado_abr
-    WHERE anio = year;
+  RETURN QUERY
+  SELECT 'State: ' || estado_desc AS category,
+       SUM(nacimientos) AS total,
+       ROUND(AVG(m_edad_prom)::numeric, 0) AS avg_age,
+       ROUND(MIN(m_edad_prom)::numeric, 0) AS min_age,
+       ROUND(MAX(m_edad_prom)::numeric, 0) AS max_age,
+       ROUND(CAST(AVG(avg_birth_weight) / 1000.0 AS numeric), 3) AS avg_weight,
+       ROUND(CAST(MIN(avg_birth_weight) / 1000.0 AS numeric), 3) AS min_weight,
+       ROUND(CAST(MAX(avg_birth_weight) / 1000.0 AS numeric), 3) AS max_weight
+FROM temp_table
+WHERE anio = aYear
+GROUP BY estado_desc
+HAVING SUM(nacimientos) > 200000
+ORDER BY estado_desc DESC;
+END;
+$$ LANGUAGE plpgsql;
 
-CREATE FUNCTION reporte_consolidado(IN n INT) RETURNS VOID AS $$
+
+/* FUNCION QUE DEVUELVE PARA UN year UN QUERY QUE TIENE LA INFO DE CADA GENERO */
+
+CREATE OR REPLACE FUNCTION year_gender_stats(aYear INTEGER)
+RETURNS TABLE (
+  category TEXT,
+  total BIGINT,
+  avgage NUMERIC,
+  minage NUMERIC,
+  maxage NUMERIC,
+  avgweight NUMERIC,
+  minweight NUMERIC,
+  maxweight NUMERIC
+)
+AS $$
+BEGIN
+  RETURN QUERY
+  SELECT 'Gender: Male' AS category,
+         SUM(nacimientos) AS total,
+         ROUND(AVG(m_edad_prom)::numeric, 0) AS AvgAge,
+         ROUND(MIN(m_edad_prom)::numeric, 0) AS MinAge,
+         ROUND(MAX(m_edad_prom)::numeric, 0) AS MaxAge,
+         ROUND(CAST(AVG(avg_birth_weight) / 1000.0 AS numeric), 3) AS AvgWeight,
+         ROUND(CAST(MIN(avg_birth_weight) / 1000.0 AS numeric), 3) AS MinWeight,
+         ROUND(CAST(MAX(avg_birth_weight) / 1000.0 AS numeric), 3) AS MaxWeight
+  FROM temp_table
+  WHERE anio = aYear AND genero = 'M'
+
+  UNION ALL
+
+  SELECT 'Gender: Female' AS category,
+         SUM(nacimientos) AS total,
+         ROUND(AVG(m_edad_prom)::numeric, 0) AS AvgAge,
+         ROUND(MIN(m_edad_prom)::numeric, 0) AS MinAge,
+         ROUND(MAX(m_edad_prom)::numeric, 0) AS MaxAge,
+         ROUND(CAST(AVG(avg_birth_weight) / 1000.0 AS numeric), 3) AS AvgWeight,
+         ROUND(CAST(MIN(avg_birth_weight) / 1000.0 AS numeric), 3) AS MinWeight,
+         ROUND(CAST(MAX(avg_birth_weight) / 1000.0 AS numeric), 3) AS MaxWeight
+  FROM temp_table
+  WHERE anio = aYear AND genero = 'F';
+
+  RETURN;
+END;
+$$ LANGUAGE plpgsql;
 
 
+/* FUNCION QUE TE DEUVELVE PARA UN year UN QUERY QUE TIENE LA INFO DE LA EDUCACION */
 
-CREATE FUNCTION es_año_bisiesto(IN n INT) RETURNS BOOLEAN AS $$
+CREATE OR REPLACE FUNCTION year_education_stats(aYear INTEGER)
+RETURNS TABLE (
+  category TEXT,
+  total BIGINT,
+  avgage NUMERIC,
+  minage NUMERIC,
+  maxage NUMERIC,
+  avgweight NUMERIC,
+  minweight NUMERIC,
+  maxweight NUMERIC
+)
+AS $$
+BEGIN
+  RETURN QUERY
+  SELECT edu_desc AS category,
+         SUM(nacimientos) AS total,
+         ROUND(AVG(m_edad_prom)::numeric, 0) AS AvgAge,
+         ROUND(MIN(m_edad_prom)::numeric, 0) AS MinAge,
+         ROUND(MAX(m_edad_prom)::numeric, 0) AS MaxAge,
+         ROUND(CAST(AVG(avg_birth_weight) / 1000.0 AS numeric), 3) AS AvgWeight,
+         ROUND(CAST(MIN(avg_birth_weight) / 1000.0 AS numeric), 3) AS MinWeight,
+         ROUND(CAST(MAX(avg_birth_weight) / 1000.0 AS numeric), 3) AS MaxWeight
+  FROM temp_table
+  WHERE anio = aYear AND edu_code != -9
+  GROUP BY edu_desc
+  ORDER BY edu_desc DESC;
+
+  RETURN;
+END;
+$$ LANGUAGE plpgsql;
+
+
+/*FUNCION QUE CALCULA LOS STATS PARA UN ANO */
+
+CREATE OR REPLACE FUNCTION year_calculate_stats(aYear INTEGER)
+RETURNS VOID
+AS $$
+DECLARE
+    row_result RECORD;
+    total_total INTEGER := 0;
+    count_total INTEGER := 0;
+    avg_avgage NUMERIC := 0;
+    avg_minage NUMERIC := 0;
+    avg_maxage NUMERIC := 0;
+    avg_avgweight NUMERIC := 0;
+    avg_minweight NUMERIC := 0;
+    avg_maxweight NUMERIC := 0;
+    result_string TEXT := '';
+BEGIN
+    FOR row_result IN (
+        SELECT *
+        FROM (
+            SELECT *
+            FROM year_state_stats(aYear)
+
+            UNION ALL
+
+            SELECT *
+            FROM year_gender_stats(aYear)
+
+            UNION ALL
+
+            SELECT *
+            FROM year_education_stats(aYear)
+        ) AS subquery
+    )
+    LOOP
+        RAISE NOTICE '%', row_result;
+        total_total := total_total + row_result.total;
+        count_total := count_total + 1;
+        avg_avgage := avg_avgage + row_result.avg_age;
+        avg_minage := avg_minage + row_result.min_age;
+        avg_maxage := avg_maxage + row_result.max_age;
+        avg_avgweight := avg_avgweight + row_result.avg_weight;
+        avg_minweight := avg_minweight + row_result.min_weight;
+        avg_maxweight := avg_maxweight + row_result.max_weight;
+    END LOOP;
+    
+    avg_avgage := ROUND(avg_avgage / count_total);
+    avg_minage := ROUND(avg_minage / count_total);
+    avg_maxage := ROUND(avg_maxage / count_total);
+    avg_avgweight := ROUND(avg_avgweight / count_total, 3);
+    avg_minweight := ROUND(avg_minweight / count_total, 3);
+    avg_maxweight := ROUND(avg_maxweight / count_total, 3);
+    
+    result_string := 'Total Aggregate: ' || total_total || ', Average AvgAge: ' || avg_avgage || ', Average MinAge: ' || avg_minage ||
+                     ', Average MaxAge: ' || avg_maxage || ', Average AvgWeight: ' || avg_avgweight || ', Average MinWeight: ' ||
+                     avg_minweight || ', Average MaxWeight: ' || avg_maxweight;
+    
+    RAISE NOTICE '%', result_string;
+END;
+$$ LANGUAGE plpgsql;
+
+
+/*FUNCION QUE RECIBE UN N E IMPRIME LOS STATS DE LOS PRIMEROS N ANIOS */
+
+CREATE OR REPLACE FUNCTION ReporteConsolidado(N INTEGER)
+RETURNS VOID
+AS $$
+DECLARE
+    year_value INTEGER;
+BEGIN
+    FOR year_value IN (
+        SELECT anio
+        FROM anio
+        ORDER BY anio
+        LIMIT N
+    )
+    LOOP
+        PERFORM year_calculate_stats(year_value);
+    END LOOP;
+END;
+$$ LANGUAGE plpgsql;
+
+
+/* PARA CORRER EL COMANDO USAR LOS SIG COMANDOS */
+
+DO $$
+BEGIN
+PERFORM ReporteConsolidado(2);
+END;
+$$ LANGUAGE PLPGSQL;
